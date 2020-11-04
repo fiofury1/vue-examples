edda016108d5daf52afdaa1d4953d62d51352477
Jon Fiorelli <jfiorelli@tesla.com>
2020-11-04 08:21:35 -0500

    Add components/props example.

---
+++ components/props/index.js
@@ -0,0 +1,16 @@
+Vue.component('ingredient-item', {
+    props: ['ingredient'],
+    template: '<li>{{ ingredient.text }}</li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      singleIngredient: {id: 0, text: "Butter"},
+      ingredientList: [
+        { id: 0, text: 'Salt' },
+        { id: 1, text: 'Pepper' },
+        { id: 2, text: 'Milk' }
+      ]
+    }
+})
