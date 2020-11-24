18ce0ccdc5c6a0f75f371e8601d8cb4a410119fa
Jon Fiorelli <jfiorelli@tesla.com>
2020-11-24 08:13:15 -0500

    Update component and v-on examples.

--- components/props/index.js
+++ components/props/index.js
@@ -1,4 +1,9 @@
-Vue.component('ingredient-item', {
+Vue.component('ingredient-item1', {
+    props: ['ingredient'],
+    template: '<li>{{ ingredient }}</li>'
+})
+
+Vue.component('ingredient-item2', {
     props: ['ingredient'],
     template: '<li>{{ ingredient.text }}</li>'
 })
@@ -6,7 +11,8 @@ Vue.component('ingredient-item', {
 var app = new Vue({
     el: '#app',
     data: {
-      singleIngredient: {text: "Butter"},
+      singleIngredient1: "Garlic",
+      singleIngredient2: {text: "Butter"},
       ingredientList: [
         { id: 0, text: 'Salt' },
         { id: 1, text: 'Pepper' },
