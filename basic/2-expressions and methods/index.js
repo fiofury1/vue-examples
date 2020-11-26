79b36a7262816bbffbc2cbba9c552d1efbace401
Jon Fiorelli <jfiorelli@tesla.com>
2020-11-26 08:03:34 -0500

    Updates.

--- basic/2-expressions and methods/index.js
+++ basic/2-expressions and methods/index.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello friends!'    
+        message: 'Hello friends!', 
+        my_num: 5 
     },
     methods: {
         excited:  function(arg) {
