9812eee924428eb6365470be88b50937b006ed4e
Jon Fiorelli <jfiorelli@tesla.com>
2020-11-30 10:27:02 -0500

    Update directives/v-if example.

--- basic/2-expressions and methods/index.js
+++ basic/2-expressions and methods/index.js
@@ -2,11 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello friends!', 
-        my_num: 5 
+        my_num: 5, 
+        counter: 0
     },
     methods: {
         excited:  function(arg) {
             return arg+"!!!";
+        },
+        incrementCounter() {
+            this.counter += 1;
         }
     }
 });
