3393c310ccd91b93e74b49548e1563542221b5dd
Jon Fiorelli <jfiorelli@tesla.com>
2020-12-03 10:18:31 -0500

    Update v-for example.

--- directives/v-for/index.js
+++ directives/v-for/index.js
@@ -1,13 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
-    title: "Property To Do List",
-    todos: [
+    todos1: [
+      'Exercise',
+      'Sleep'
+    ],
+    todos2: [
       { text: 'Walk Dog' },
       { text: 'Wash Car' },
-      { text: 'Make Dinner' }
     ],
-    title1: "Method To Do List",
     todo1:  "Do Laundry",
     todo2:  "Wash Dishes",
     todo3:  "Make Pie",
