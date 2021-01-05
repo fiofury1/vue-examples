1969219277ff95a64201b2e05155a5a5a93d415f
Jon Fiorelli <jfiorelli@tesla.com>
2021-01-05 09:30:07 -0500

    Create conditional rendering folder.  Update v-if/v-show example.

--- directives/conditional rendering (v-if, v-show)/index.js
+++ directives/conditional rendering (v-if, v-show)/index.js
@@ -1,6 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
+    isError: true,
+    errorMessage: "You have an error",
     name: "Joe",
     enter_message: "Enter",
     do_not_enter_message: "Do not enter"
