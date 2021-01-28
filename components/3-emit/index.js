cf865cd8a03f01dfb10490c4bffe9cb0772a6b66
Jon Fiorelli <jfiorelli@tesla.com>
2021-01-28 11:43:49 -0500

    Add example showing how to pass an argument when using $emit().

--- components/3-emit/index.js
+++ components/3-emit/index.js
@@ -1,3 +1,11 @@
+
+
+// Example 1
+// - We add an on-click event to the button
+// - The button triggers a method that triggers the 'increment' event.
+// - In our html the parent is listening for the 'increment' event.
+// - The 'increment' event triggers the running of the 'incrementCounter' method.
+// - The 'incrementCounter' method updates the property 'data', which dynamically updates on our page.
 Vue.component('incrementer', {
     template: '<p><button @click="emitIncrementSignal">Increment</button></p>',
     methods: {
@@ -7,15 +15,44 @@ Vue.component('incrementer', {
     }
 })
 
+// Example 2
+// - In this example, we show how to pass a value using $emit.
+Vue.component('random-thought-box', {
+    template:`
+        <div>
+            <input type="text" 
+                   placeholder="Enter a random thought"
+                   v-model="new_thought"
+                   @keyup.enter="addThoughtSignal">
+            </input>
+        </div>`,
+    data() {
+        return {
+            new_thought: null,
+        }
+    },
+    methods: {
+        addThoughtSignal() {
+            this.$emit("add-thought", this.new_thought)
+            this.new_thought=null;
+
+        }
+    }
+})
+
 
 var app = new Vue({
     el: '#app',
     data: {
-        counter: 0
+        counter: 0,
+        thoughts: []
     },
     methods: {
         incrementCounter() {
             this.counter+=1;
+        },
+        addThought(new_thought){
+            this.thoughts.push(new_thought)
         }
     }
 })
